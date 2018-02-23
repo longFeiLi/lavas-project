3bfd2432b88125e928a2ec90022dac31d20f5c6b
luke.li <luke.li@sunvalley.com.cn>
2018-02-23 14:45:29 +0800

    213

---
+++ pages/Index.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="page-index">
+        <h2>LAVAS</h2>
+        <h4>test环境</h4>
+         <!-- link to index -->
+        <router-link to="/detail/1">Back to Detail</router-link>
+    </div>
+</template>
+
+<script>
+function setState(store) {}
+
+export default {
+    name: 'index',
+    metaInfo: {
+        title: 'Home',
+        titleTemplate: '我的标题',
+        meta: [
+            {name: 'keywords', content: 'lavas PWA'},
+            {name: 'description', content: '解决各种电影票价问题'}
+        ]
+    },
+    async asyncData({store, route}) {
+        setState(store);
+    }
+};
+</script>
+
+<style lang="stylus" scoped>
+
+.page-index
+    display flex
+    justify-content center
+    align-items center
+    flex-direction column
+
+    h2
+        font-size 46px
+        font-weight 500
+        margin-bottom 0
+
+</style>
---
+++ pages/detail/_id.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="detail-wrapper">
+        <p></p>
+        <!-- link to another detail -->
+        <router-link :to="{
+            name: 'detailId',
+            params: {
+                id: Number($route.params.id) + 1
+            }
+        }">下一页 {{Number($route.params.id) + 1}}</router-link>
+        <article class="detail-content">
+            <header class="detail-title">
+                Detail {{$route.params.id}}
+            </header>
+            <!-- link to another detail -->
+            <p>
+            Progressive Web Apps are user experiences that have the reach of the web, and are:
+Reliable - Load instantly and never show the downasaur, even in uncertain network conditions.
+Fast - Respond quickly to user interactions with silky smooth animations and no janky scrolling.
+            </p>
+            <!-- link to index -->
+        </article>
+
+
+
+        <!-- link to index -->
+        <router-link to="/">Back to home</router-link>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'detail-_id',
+    metaInfo() {
+        return {
+            title: `Lavas Sample Detail ${this.$route.params.id}`,
+            titleTemplate: '%s - Lavas',
+            meta: [
+                {name: 'keywords', content: `Lavas Sample Detail`},
+                {name: 'description', content: `Lavas Sample Detail ${this.$route.params.id}`}
+            ]
+        }
+    }
+};
+</script>
+
+<style lang="stylus" scoped>
+.detail-content
+    font-size 16px
+    line-height 30px
+    margin-top 30px
+
+    .detail-title
+        margin-bottom 20px
+        padding 10px 0
+        font-size 36px
+        font-weight bold
+
+</style>
